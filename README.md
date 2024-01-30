0c25e280848db8b9aa287d56f871ebf8baae6cdb
Alujack <125638350+Alujack@users.noreply.github.com>
2024-01-30 12:18:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Alujack.github.io
---
+++ index.js
@@ -0,0 +1,53 @@
+let Menu_icon= document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+Menu_icon.onclick = () => {
+    Menu_icon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+let sections=document.querySelectorAll("section");
+let navlink=document.querySelectorAll("header nav a");
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top= window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+        if(top >=offset && top<= offset+height){
+            navlink.forEach(link =>{
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+            });
+        };
+
+    });
+    let  header= document.querySelector('.header')
+        header.classList.toggle('sticky' ,window.scrollY > 100);
+    Menu_icon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+ScrollReveal({ 
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-image, .service-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .image-about', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+
+const type = new Typed(".multyTyping" ,{
+strings: ['Frontend Developer', 'FullStack developer', 'Software Engineer', 'Cyber Security', 'IT Support'],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop: true
+});
